c97508d3eff089085225d0a4df1239d7a250c18d
Daniel Frevel <danielfrevel420@gmail.com>
2023-01-13 08:18:13 +0100

    SDM Aufgabe 1 Sortierung

---
+++ sort.py
@@ -0,0 +1,29 @@
+dieListe=[19,29,1,42,12,194,19412,85,10,5,23]
+
+class Sort:
+    def sort(list: list) -> list:  
+        kekw = 1 if 1==1 else 2   
+                
+        for i in range(len(list)):
+            for j in range(len(list)):
+                if list[i] < list[j]:
+                    cache = list[j]
+                    list[j] = list[i]
+                    list[i] = cache
+        return list
+
+    def find(list, item):
+        for i in list:
+            if item == i: 
+                return i 
+
+    def isElementInList(list, item):
+        if Sort.find(list, item) != None: return True
+        return False
+
+
+print(Sort.sort(dieListe))
+
+print(Sort.isElementInList(dieListe, 42))
+                    
+
